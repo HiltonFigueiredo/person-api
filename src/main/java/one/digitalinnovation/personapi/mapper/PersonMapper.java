4a0440ca828536f11322177587ffb80da524d7eb
HiltonFigueiredo <hiltonfigueiredobarbosa@gmail.com>
2021-09-23 15:42:08 -0300

    Developed Endpoint List All People

--- src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
+++ src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
@@ -14,4 +14,5 @@ public interface PersonMapper {
     @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
     Person toModel(PersonDTO personDTO);
 
+    PersonDTO toDTO(Person person);
 }
